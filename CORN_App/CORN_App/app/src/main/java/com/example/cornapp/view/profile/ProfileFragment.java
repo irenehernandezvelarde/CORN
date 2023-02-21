d01188215daee4711a9c5c57855d9f11727731fb
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2023-02-21 17:08:26 +0100

    //Cambiar perfil, siempre valores editables

--- CORN_App/CORN_App/app/src/main/java/com/example/cornapp/view/profile/ProfileFragment.java
+++ CORN_App/CORN_App/app/src/main/java/com/example/cornapp/view/profile/ProfileFragment.java
@@ -18,38 +18,16 @@ public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
     private ProfileViewModel viewModel;
 
-    private boolean userEditing = false;
-    private boolean contactEditing = false;
-    private boolean emailEditing = false;
-
-
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
 
-        setupProfileUI();
         setupListeners();
-        setupObservers();
         viewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         return binding.getRoot();
     }
 
-    private void setupProfileUI() {
-        binding.profileUserNameValue.setFocusable(false);
-        binding.profileUserSurnameValue.setFocusable(false);
-        binding.profileEmailEdit.setFocusable(false);
-        binding.profileContactEdit.setFocusable(false);
-    }
 
     public void setupListeners(){
-        binding.profileUserEdit.setOnClickListener(view -> {
-            setUserEditable(!userEditing);
-        });
-        binding.profileContactEdit.setOnClickListener(view -> {
-            setContactEditable(!contactEditing);
-        });
-        binding.profileEmailCardview.setOnClickListener(view -> {
-            setEmailEditable(!emailEditing);
-        });
         binding.fab.setOnClickListener(view -> {
             viewModel.updateUser(
                     binding.profileUserNameValue.getText(),
@@ -58,28 +36,6 @@ public class ProfileFragment extends Fragment {
                     binding.profileEmailEditValue.getText()
             );
         });
-    }
-
-    public void setupObservers(){
-        final Observer<UserBo> nameObserver = _user -> {
-            Log.d("5cos", _user.toString());
-        };
-    }
-
-    private void setEmailEditable(boolean state) {
-        binding.profileEmailEdit.setFocusable(state);
-        emailEditing = state;
-    }
-
-    private void setContactEditable(boolean state) {
-        binding.profileContactEdit.setFocusable(state);
-        contactEditing = state;
-    }
-
-    private void setUserEditable(boolean state) {
-        binding.profileUserNameValue.setFocusable(state);
-        binding.profileUserSurnameValue.setFocusable(state);
-        userEditing = state;
     }
 
 }
