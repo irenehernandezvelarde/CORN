9925f1e14610073f4307ced28c58298ca8748a33
pmunuera <95918506+pmunuera@users.noreply.github.com>
2023-02-24 16:08:01 +0100

    Commit N' Push

--- CORN_App/CORN_App/app/src/main/java/com/example/cornapp/UtilsHTTP.java
+++ CORN_App/CORN_App/app/src/main/java/com/example/cornapp/UtilsHTTP.java
@@ -4,92 +4,108 @@ import android.os.Handler;
 import android.os.Looper;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
-import javax.net.ssl.HttpsURLConnection;
 
 public class UtilsHTTP {
-        public static void sendGET(String url, Consumer<String> callBack) {
-            send("GET", url, "", callBack);
-        }
 
-        public static void sendPOST(String url, String post_params, Consumer<String> callBack) {
-            send("POST", url, post_params, callBack);
-        }
+    public static void sendGET(String url, Consumer<String> callBack) {
+        send("GET", url, "", "", callBack);
+    }
 
-        private static void send(String type, String url, String post_params, Consumer<String> callBack) {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
+    public static void sendGET(String url, String cookie, Consumer<String> callBack) {
+        send("GET", url, cookie, "", callBack);
+    }
 
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
+    public static void sendPOST(String url, String post_params, Consumer<String> callBack) {
+        send("POST", url, "", post_params, callBack);
+    }
 
-                    // Tasques en background (xarxa)
-                    try {
-                        URL obj = new URL(url);
-                        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-                        con.setRequestMethod(type);
-                        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+    public static void sendPOST(String url, String cookie, String post_params, Consumer<String> callBack) {
+        send("POST", url, cookie, post_params, callBack);
+    }
 
-                        if (type.equals("POST")) {
-                            con.setDoOutput(true);
-                            OutputStream os = con.getOutputStream();
-                            os.write(post_params.getBytes());
-                            os.flush();
-                            os.close();
-                        }
+    private static void send(String type, String url, String cookie, String post_params, Consumer<String> callBack) {
 
-                        int responseCode = con.getResponseCode();
-
-                        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-                            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                            String inputLine;
-                            StringBuffer response = new StringBuffer();
-
-                            while ((inputLine = in.readLine()) != null) {
-                                response.append(inputLine);
-                            }
-                            in.close();
-
-                            //return response.toString();
-                            System.out.println(response);
-                            callBack.accept(response.toString());
-                        } else {
-                            System.out.println(type + " request did not work.");
-                        }
-                    } catch (Exception e) {
-                        System.out.println(type + " request error.");
-                        System.out.println(e);
-
+        // Create a new thread to send the request
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                String errorStr = "{ \"status\": \"KO\", \"result\": \"Error on " + type + " request\" }";
+                try {
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    con.setRequestMethod(type);
+                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    if (!cookie.equals("")) {
+                        con.setRequestProperty("Cookie", cookie);
                     }
-                    //return "{ \"status\": \"KO\", \"result\": \"Error on " + type + " request\" }";
-                    callBack.accept("{ \"status\": \"KO\", \"result\": \"Error on " + type + " request\" }");
 
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
+                    if (type.equals("POST")) {
+                        con.setDoOutput(true);
+                        OutputStream os = con.getOutputStream();
+                        os.write(post_params.getBytes());
+                        os.flush();
+                        os.close();
+                    }
 
-                            // Tasques a la interfície gràfica (GUI)
+                    int responseCode = con.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                        String inputLine;
+                        StringBuffer response = new StringBuffer();
 
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
                         }
-                    });
+                        in.close();
+                        callBackHandler(response.toString(), callBack);
+
+                    } else {
+                        System.out.println(type + " request did not work.");
+                        callBackHandler(errorStr, callBack);
+                    }
+                } catch (Exception e) {
+                    System.out.println(type + " request error.");
+                    callBackHandler(errorStr, callBack);
                 }
-            });            // Create a new thread to send the request
+            }
+        });
+    }
 
+    static private void callBackHandler(String result, Consumer<String> callBack) {
 
-        }
+        // Code for JavaFX
+        /*
+        Platform.runLater(()->{
+            callBack.accept(result);
+        });
+        */
+
+        // Code for Android
+        new Handler(Looper.getMainLooper()).post(new Runnable () {
+            @Override
+            public void run () {
+                callBack.accept(result);
+            }
+        });
+
+        // Code for Swing
+        /*
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                callBack.accept(result);
+            }
+        });
+        */
+    }
 }
-
 
 
--- CORN_App/CORN_App/app/src/main/java/com/example/cornapp/view/payment/PaymentFragment.java
+++ CORN_App/CORN_App/app/src/main/java/com/example/cornapp/view/payment/PaymentFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cornapp.UtilsHTTP;
 import com.example.cornapp.databinding.FragmentPaymentBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PaymentFragment extends Fragment {
 
@@ -18,6 +24,40 @@ public class PaymentFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPaymentBinding.inflate(inflater, container, false);
         return binding.getRoot();
-    }
 
+
+    }
+    public void generateQR(){
+        binding.button.setOnClickListener(view -> {
+
+            JSONObject obj = null;
+            try {
+                obj = new JSONObject("{}");
+                obj.put("type", "sync");
+
+                UtilsHTTP.sendPOST("http" + "://" + "10.0.2.2:" + 3000 + "/dades", obj.toString(), (response) -> {
+                    JSONObject objResponse = null;
+                    try {
+                        objResponse = new JSONObject(response);
+                        System.out.println(response);
+                        if (objResponse.getString("status").equals("OK")) {
+                            JSONArray JSONlist = objResponse.getJSONArray("result");
+                            JSONObject user = null;
+                            for (int i = 0; i < JSONlist.length(); i++) {
+                                // Get console information
+                                user = JSONlist.getJSONObject(i);
+                                // Fill template with console information
+                                System.out.println(user);
+                            }
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
