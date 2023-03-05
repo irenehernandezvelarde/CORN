ec511788146ccb07f330e0764424e61684c9efc7
pmunuera <pmunueragarcia.cf@iesesteveterradas.cat>
2023-03-05 22:11:35 +0100

    Commit N' Push 2

--- CORN_App/CORN_App/app/src/main/java/com/example/cornapp/SignUp.java
+++ CORN_App/CORN_App/app/src/main/java/com/example/cornapp/SignUp.java
@@ -11,8 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SignUp extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class SignUp extends AppCompatActivity {
+    public String userToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +32,46 @@ public class SignUp extends AppCompatActivity {
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
-                if (gmail.getText().toString().equals("hola")) {
+
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject("{}");
+                    obj.put("type", "signup");
+                    obj.put("phone", telefon.getText().toString());
+                    obj.put("email", gmail.getText().toString());
+                    obj.put("name", nom.getText().toString());
+                    obj.put("surname", cognom.getText().toString());
+                    if(contra.getText().toString().equalsIgnoreCase(repitContra.getText().toString())) {
+                        obj.put("password", contra.getText().toString());
+                    }
+
+                    UtilsHTTP.sendPOST("http" + "://" + "localhost:" + 3000 + "/dades", obj.toString(), (response) -> {
+                        JSONObject objResponse = null;
+                        try {
+                            objResponse = new JSONObject(response);
+                            System.out.println(response);
+                            if (objResponse.getString("status").equals("OK")) {
+                                JSONArray JSONlist = objResponse.getJSONArray("result");
+                                JSONObject user = null;
+                                for (int i = 0; i < JSONlist.length(); i++) {
+                                    user = JSONlist.getJSONObject(i);
+                                    userToken = user.getString("token");
+                                    if(objResponse.getString("message").equals("created")) {
+                                        startActivity(new Intent(SignUp.this, MainActivity.class));
+                                    }
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                /*if (gmail.getText().toString().equals("hola")) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
                     builder.setTitle("Hola!");
                     builder.setMessage("Perfecto");
@@ -41,7 +84,7 @@ public class SignUp extends AppCompatActivity {
 
                 } else {
                     startActivity(new Intent(SignUp.this,Login.class));
-                }
+                }*/
 
             }
         });
